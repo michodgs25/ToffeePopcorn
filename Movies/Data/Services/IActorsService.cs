b0d49409d8a1082c675e057dd51b3e4f44942a7f
Michael Hodgson <michael.hodgson@springboardit.co.uk>
2022-02-09 12:57:03 +0000

    Add Iservice interface file for Actor data

---
+++ Movies/Data/Services/IActorsService.cs
@@ -0,0 +1,18 @@
+﻿using Movies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+
+        void Delete(int id);
+    }
+}
