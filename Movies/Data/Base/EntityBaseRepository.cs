d38e98977dc9819cea76845dd1a74c95d276bcfb
Michael Hodgson <michael.hodgson@springboardit.co.uk>
2022-02-22 15:09:21 +0000

    Add update Async& DeleteAsyc in base repository

--- Movies/Data/Base/EntityBaseRepository.cs
+++ Movies/Data/Base/EntityBaseRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -15,31 +16,23 @@ namespace Movies.Data.Base
             _context = context; 
         }
 
-        public Task AddAsync(T entity)
+        public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
+
+        public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var entity = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Deleted;
         }
 
-        public Task DeleteAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
 
-        public async Task<IEnumerable<T>> GetAllAsync()
-        {
-            var result = await _context.Set<T>().ToListAsync();
-            return result;
-        }
+        public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
 
-        public async Task<T> GetByIdAsync(int id)
+        public async Task UpdateAsync(int id, T entity)
         {
-            var result = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
-            return result;
-        }
-
-        public Task<T> UpdateAsync(int id, T entity)
-        {
-            throw new NotImplementedException();
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Modified;
         }
     }
 }
